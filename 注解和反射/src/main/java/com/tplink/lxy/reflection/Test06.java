a9654a3bf04be0b8e22e71fa1522f6756e808497
xiaoying1 <linxiaoying1@tp-link.com.cn>
2020-09-28 15:16:07 +0800

    注解和反射

--- 注解和反射/src/main/java/com/tplink/lxy/reflection/Test06.java
+++ 注解和反射/src/main/java/com/tplink/lxy/reflection/Test06.java
@@ -4,9 +4,21 @@ public class Test06 {
     static {
         System.out.println("Main类被加载");
     }
-    public static void main(String[] args) {
-        Son son = new Son();
+    public static void main(String[] args) throws ClassNotFoundException {
+        //主动引用
+//        Son son = new Son();
+        //反射也会产生主动引用
+//        Class.forName("com.tplink.lxy.reflection.Son");
 
+
+
+        //不会产生类的引用的方法
+        //通过子类引用父类的静态变量，不会导致子类初始化
+        System.out.println(Son.b);
+        //通过数组定义类引用时，不会触发类的初始化
+        Son[] array = new Son[5];
+        //引用常量不会触发此类的初始化（常量在链接阶段就存入调用类的常量池中了）
+        System.out.println(Son.M);
     }
 }
 
