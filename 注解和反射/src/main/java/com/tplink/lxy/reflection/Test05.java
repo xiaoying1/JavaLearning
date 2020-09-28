10623af68fd8e2e693507ff6758c806163587f64
xiaoying1 <linxiaoying1@tp-link.com.cn>
2020-09-28 10:50:14 +0800

    注解和反射

--- 注解和反射/src/main/java/com/tplink/lxy/reflection/Test05.java
+++ 注解和反射/src/main/java/com/tplink/lxy/reflection/Test05.java
@@ -4,6 +4,16 @@ public class Test05 {
     public static void main(String[] args) {
         A a = new A();
         System.out.println(A.m);
+        /*
+        1.加载到内存，会产生一个类对应的class对象
+        2.链接，链接结束后m=0
+        3.初始化
+         <clinit>(){
+            System.out.println("A类静态代码块初始化");
+            m = 300;
+            m =100;
+         }
+         */
     }
 }
 class A{
@@ -11,7 +21,7 @@ class A{
         System.out.println("A类静态代码块初始化");
         m = 300;
     }
-    static int m =100;
+    static int m = 100;
 
     public A(){
         System.out.println("A类的无参构造初始化");
