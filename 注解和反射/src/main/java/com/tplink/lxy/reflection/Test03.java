eb6f0f139283e43883352ef57afeb696022a8fec
xiaoying1 <linxiaoying1@tp-link.com.cn>
2020-09-28 14:49:22 +0800

    注解和反射

---
+++ 注解和反射/src/main/java/com/tplink/lxy/reflection/Test03.java
@@ -0,0 +1,54 @@
+package com.tplink.lxy.reflection;
+
+public class Test03 {
+    public static void main(String[] args) throws ClassNotFoundException {
+        Person person = new Student();
+        System.out.println("这个人是："+person.name);
+
+        //方式一：通过对象获取
+        Class c1 = person.getClass();
+        System.out.println(c1.hashCode());
+
+        //方式二：forname获得
+        Class c2 = Class.forName("com.tplink.lxy.reflection.Student");
+        System.out.println(c2.hashCode());
+
+        //方式三：类名.class获得
+        Class c3 = Student.class;
+        System.out.println(c3.hashCode());
+
+        //方式四：基本内置类型的包装类都有一个TYPE属性
+        Class c4 = Integer.TYPE;
+        System.out.println(c4);
+
+        Class c5 = c1.getSuperclass();
+        System.out.println(c5);
+    }
+}
+class Person{
+    String name;
+
+    public Person(){
+
+    }
+    public Person(String name){
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
+class Student extends Person{
+    public Student(){
+        this.name = "学生";
+    }
+}
+class Teacher extends Person{
+    public Teacher(){
+        this.name = "老师";
+    }
+}
---
+++ 注解和反射/src/main/java/com/tplink/lxy/reflection/Test04.java
@@ -0,0 +1,28 @@
+package com.tplink.lxy.reflection;
+
+import java.lang.annotation.ElementType;
+
+public class Test04 {
+    public static void main(String[] args) {
+        Class c1 = Object.class;//类
+        Class c2 = Comparable.class;//接口
+        Class c3 = String[].class;//一维数组
+        Class c4 = int[][].class;//二维数组
+        Class c5 = void.class;//void
+        Class c6 = Override.class;//注解
+        Class c7 = ElementType.class;//枚举
+        Class c8 = Integer.class;//基本数据类型
+        Class c9 = Class.class;//Class
+
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+        System.out.println(c4);
+        System.out.println(c5);
+        System.out.println(c6);
+        System.out.println(c7);
+        System.out.println(c8);
+        System.out.println(c9);
+
+    }
+}
