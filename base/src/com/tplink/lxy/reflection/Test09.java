08e0955f95817ac42708c491b32b8e3c033be9b0
xiaoying1 <linxiaoying1@tp-link.com.cn>
2020-09-29 11:16:10 +0800

    update

---
+++ base/src/com/tplink/lxy/reflection/Test09.java
@@ -0,0 +1,18 @@
+package com.tplink.lxy.reflection;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+public class Test09 {
+    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException, InvocationTargetException, NoSuchMethodException {
+        Class c1 = Class.forName("com.tplink.lxy.reflection.User");
+
+        User user = (User)c1.newInstance();//本质是调用无参构造器
+        System.out.println(user);
+
+        //通过构造器创建对象
+        Constructor constructor = c1.getDeclaredConstructor(String.class, int.class, int.class);
+        User user1 = (User) constructor.newInstance("lxy",001,18);
+        System.out.println(user1);
+    }
+}
