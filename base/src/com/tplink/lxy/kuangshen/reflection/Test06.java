9a592ba6c47e1f9ab31fc5bf8054bc5dde264b94
xiaoying1 <linxiaoying1@tp-link.com.cn>
2020-09-30 15:23:41 +0800

    update

---
+++ base/src/com/tplink/lxy/kuangshen/reflection/Test06.java
@@ -0,0 +1,40 @@
+package com.tplink.lxy.kuangshen.reflection;
+
+public class Test06 {
+    static {
+        System.out.println("Main类被加载");
+    }
+    public static void main(String[] args) throws ClassNotFoundException {
+        //主动引用
+//        Son son = new Son();
+        //反射也会产生主动引用
+//        Class.forName("com.tplink.lxy.kuangshen.reflection.Son");
+
+
+
+        //不会产生类的引用的方法
+        //通过子类引用父类的静态变量，不会导致子类初始化
+        System.out.println(Son.b);
+        //通过数组定义类引用时，不会触发类的初始化
+        Son[] array = new Son[5];
+        //引用常量不会触发此类的初始化（常量在链接阶段就存入调用类的常量池中了）
+        System.out.println(Son.M);
+    }
+}
+
+class Father{
+    static int b=2;
+
+    static {
+        System.out.println("父类被加载");
+    }
+
+}
+class Son extends Father {
+    static {
+        System.out.println("子类被加载");
+        m = 300;
+    }
+    static int m = 100;
+    static final int M = 1;
+}
---
+++ base/src/com/tplink/lxy/kuangshen/reflection/Test08.java
@@ -0,0 +1,61 @@
+package com.tplink.lxy.kuangshen.reflection;
+
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+public class Test08 {
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchFieldException, NoSuchMethodException {
+        Class c1 = Class.forName("com.tplink.lxy.reflection.User");
+
+        //获得类的名字
+        System.out.println(c1.getName());
+        System.out.println(c1.getSimpleName());
+
+        System.out.println("==========================");
+        //获得类的属性
+        Field[] fields = c1.getFields();//只能找到public属性
+        for(Field field:fields){
+            System.out.println(field);
+        }
+        fields = c1.getDeclaredFields();//找到全部属性
+        for(Field field:fields){
+            System.out.println(field);
+        }
+        //获得指定属性
+        System.out.println(c1.getDeclaredField("name"));
+
+        //获得类的方法
+        System.out.println("==========================");
+        Method[] methods = c1.getMethods();//获得本类及其父类的全部public方法
+        for (Method method:methods){
+            System.out.println("getMethods:"+method);
+        }
+        methods = c1.getDeclaredMethods();//获得本类的所有方法
+        for (Method method:methods){
+            System.out.println("getDeclaredMethods:"+method);
+        }
+
+        //获得指定方法
+        System.out.println("==========================");
+        Method getName = c1.getMethod("getName",null);
+        Method setName = c1.getMethod("setName", String.class);
+        System.out.println(getName);
+        System.out.println(setName);
+
+
+        //获得构造器
+        System.out.println("==========================");
+        Constructor[] constructors = c1.getConstructors();
+        for(Constructor constructor:constructors){
+            System.out.println("getConstructors:"+constructor);
+        }
+        constructors = c1.getDeclaredConstructors();
+        for(Constructor constructor:constructors){
+            System.out.println("getDeclaredConstructors:"+constructor);
+        }
+
+    }
+}
+
